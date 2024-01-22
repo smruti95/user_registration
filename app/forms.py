32e4c3ff68b2374d0fcbebe0859a812249dcdab2
SMRUTI <smpatra35@gmail.com>
2024-01-22 18:37:11 +0530

    registration added

--- app/forms.py
+++ app/forms.py
@@ -6,6 +6,7 @@ class UserForm(forms.ModelForm):
      class Meta:
         model=User
         fields=['username','email','password']
+        widgets={'password':forms.PasswordInput}
         help_texts={'username':''}
 
 class ProfileForm(forms.ModelForm):
--- app/models.py
+++ app/models.py
@@ -9,3 +9,6 @@ class Profile(models.Model):
     username=models.OneToOneField(User,on_delete=models.CASCADE)
     address=models.TextField()
     profile_pic=models.ImageField()
+
+    def __str__(self):
+        return self.username
